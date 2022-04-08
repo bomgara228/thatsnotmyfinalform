42c1b444cdb56eba37d85626c6047051f49eb157
bomgara228 <92594974+bomgara228@users.noreply.github.com>
2022-04-08 19:35:36 +0300

    Add files via upload

---
+++ player.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#include "settings.h"
+
+class Player {
+private:
+	sf::Texture texture;
+	sf::Texture zizni;
+	sf::Sprite sprite;
+	sf::Sprite live;
+	int hp = 125;
+	int amount = 0;
+	int rasstoyan = 440;
+	float damage = 1;
+public:
+	Player();
+	void update();
+	void draw(sf::RenderWindow& window);
+	sf::FloatRect getHitBox();
+	sf::Vector2f getPosition();
+	void decreaseHp(size_t);
+	void increaseHp(size_t);
+	int getHp();
+	int dmg();
+	void increaseDamage(float);
+};
